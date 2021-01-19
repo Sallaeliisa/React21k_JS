ee9c1b3ab7cb55b3338cc9333fc1e0a7436317f7
Vuorikko Salla <s2100129@edu.bc.fi>
2021-01-19 15:51:38 +0200

    Tasks

---
+++ 2_task_calculator/script.js
@@ -0,0 +1,6 @@
+let number1 = prompt("Give a number");
+let number2 = prompt("Give another number");
+let operator = prompt("Select an operator");
+alert(Number(number1) + Number(number2));
+
+
---
+++ 3_age/script.js
@@ -0,0 +1,6 @@
+let age = prompt("How old are you?");
+if (age < 18) {
+    alert("You are too young");
+} else {
+    alert("welcome");
+}
