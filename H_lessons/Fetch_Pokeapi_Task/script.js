13650fe4457b8f285f914eeb94090d08ca955cb6
Vuorikko Salla <s2100129@edu.bc.fi>
2021-03-04 16:38:38 +0200

    images are showing

--- H_lessons/Fetch_Pokeapi_Task/script.js
+++ H_lessons/Fetch_Pokeapi_Task/script.js
@@ -17,21 +17,18 @@ fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000")
 function addPokemon(pokemon) {
     const newEl = document.createElement("div");
     newEl.className = "pokemon";
-    newEl.innerHTML = `<a href="#">${pokemon.name}</a>`;
+    newEl.innerHTML = `<div class="name"><a href="#">${pokemon.name}</a></div>`;
     document.getElementById("pokemonlist").appendChild(newEl);
     newEl.addEventListener("click", function() {
         fetch(pokemon.url)
         .then(response => response.json())
         .then(json => {
-            console.log(json.sprites.front_default)})
+            let link = json.sprites.front_default;
+            newEl.innerHTML = `<div class="image">
+            <img src="${link}"/>
+        </div>`;}
+            )
     });
-}
-
-myFunction = () => {
-    console.log(newEl);
-    // fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000")
-    // .then(response => response.json())
-    // .then(json => console.log(json));
 }
 
 
