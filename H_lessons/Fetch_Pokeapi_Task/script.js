e29101846171d1db1175b469507fbe97cb689727
Vuorikko Salla <s2100129@edu.bc.fi>
2021-03-04 17:47:45 +0200

    cleaned code

--- H_lessons/Fetch_Pokeapi_Task/script.js
+++ H_lessons/Fetch_Pokeapi_Task/script.js
@@ -1,35 +1,29 @@
-// Anonymous function
+(function () {
+  document.addEventListener("DOMContentLoaded", displayNames);
 
-(function(){
-document.addEventListener("DOMContentLoaded", displayNames)
+  function displayNames() {
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000")
+      .then((response) => response.json())
+      .then((json) => {
+        json.results.forEach(addPokemon);
+      });
+  }
 
-const pokemons = [];
-
-function displayNames() {
-fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000")
-.then(response => response.json())
-.then(json => {
-    json.results.forEach(addPokemon)
-})
-
-}
-
-function addPokemon(pokemon) {
+  function addPokemon(pokemon) {
     const newEl = document.createElement("div");
     newEl.className = "pokemon";
     newEl.innerHTML = `<div class="name"><a href="#">${pokemon.name}</a></div>`;
     document.getElementById("pokemonlist").appendChild(newEl);
-    newEl.addEventListener("click", function() {
-        fetch(pokemon.url)
-        .then(response => response.json())
-        .then(json => {
-            let link = json.sprites.front_default;
-            newEl.innerHTML = `<div class="image">
+    newEl.addEventListener("click", function () {
+      fetch(pokemon.url)
+        .then((response) => response.json())
+        .then((json) => {
+          let link = json.sprites.front_default;
+          newEl.innerHTML = `<div class="name"><a href="#">${pokemon.name}</a></div>
+          <div class="image">
             <img src="${link}"/>
-        </div>`;}
-            )
+        </div>`;
+        });
     });
-}
-
-
+  }
 })();
