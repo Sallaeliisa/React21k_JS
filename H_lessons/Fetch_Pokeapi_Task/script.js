2628631031a2f4c0d6a452444387aad296b6d2e7
Vuorikko Salla <s2100129@edu.bc.fi>
2021-03-04 16:06:37 +0200

    list of pokemons

--- H_lessons/Fetch_Pokeapi_Task/script.js
+++ H_lessons/Fetch_Pokeapi_Task/script.js
@@ -1,14 +1,38 @@
-let pokemons = [];
+// Anonymous function
 
+(function(){
+document.addEventListener("DOMContentLoaded", displayNames)
+
+const pokemons = [];
+
+function displayNames() {
 fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000")
 .then(response => response.json())
-.then(json => console.log(json));
+.then(json => {
+    json.results.forEach(addPokemon)
+})
 
-let addAnchor = () => {
-    pokemons.forEach((el) => {
-    let newAnchor = document.createElement("a");
-    newAnchor.innerHTML = "<a>" + el + "</a>";
-    document.getElementById("anchors").appendChild(newAnchor);
-})};
+}
 
-addAnchor();
+function addPokemon(pokemon) {
+    const newEl = document.createElement("div");
+    newEl.className = "pokemon";
+    newEl.innerHTML = `<a href="#">${pokemon.name}</a>`;
+    document.getElementById("pokemonlist").appendChild(newEl);
+    newEl.addEventListener("click", function() {
+        fetch(pokemon.url)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json.sprites.front_default)})
+    });
+}
+
+myFunction = () => {
+    console.log(newEl);
+    // fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000")
+    // .then(response => response.json())
+    // .then(json => console.log(json));
+}
+
+
+})();
