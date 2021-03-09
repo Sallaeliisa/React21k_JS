0ec3b46f6695ccc66a21809aa2f3e51c5eee48a3
Vuorikko Salla <s2100129@edu.bc.fi>
2021-03-09 15:04:40 +0200

    update task

--- M_lessons/Speed_game/script.js
+++ M_lessons/Speed_game/script.js
@@ -1,25 +1,105 @@
-let body = document.querySelector("body")
-let circles = document.querySelectorAll(".circle");
-let stopBtn = document.getElementById("stopBtn");
+let buttons = document.querySelectorAll(".circle");
+let scoredisplay = document.querySelector("#score");
+let overlay = document.getElementById("result");
+let gameover = document.getElementById("gameover");
+let endBtn = document.getElementById("end");
+let startBtn = document.getElementById("start");
+
+let close = document.getElementById("close");
 
 let score = 0;
+let active = 0;
+let time = 1000;
 
-console.log(circleArray[2]);
+// Make buttons unclickable and hide stop button
 
+buttons[0].classList.toggle("inactive");
+buttons[1].classList.toggle("inactive");
+buttons[2].classList.toggle("inactive");
+buttons[3].classList.toggle("inactive");
+endBtn.style.visibility = "hidden";
 
-const clickFunction = () => {
-    console.log(circleArray[this.Number]);
-    score++;
-    console.log(score);
+// Start game, make buttons clickable again, pick an active button
+
+const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const startGame = () => {
+    console.log("Game started");
+
+    buttons[0].classList.remove("inactive");
+    buttons[1].classList.remove("inactive");
+    buttons[2].classList.remove("inactive");
+    buttons[3].classList.remove("inactive");
+    startBtn.style.visibility = "hidden";
+    endBtn.style.visibility = "visible";
+  
+    let nextActive = pickNew(active);
+  
+    buttons[nextActive].classList.toggle("active");
+    buttons[active].classList.remove("active");
+  
+    active = nextActive;
+  
+    console.log("Active:", active);
+  
+    timer = setTimeout(startGame, time);
+  
+    function pickNew(active) {
+      let nextActive = getRandomInt(0, 3);
+  
+      if (nextActive != active) {
+        return nextActive;
+      } else {
+        return pickNew(active);
+      }
+    }
+
+    // Identify which button was clicked
+
+    buttons[0].onclick = function () {
+        clicked(0);
+      };
+      buttons[1].onclick = function () {
+        clicked(1);
+      };
+      buttons[2].onclick = function () {
+        clicked(2);
+      };
+      buttons[3].onclick = function () {
+        clicked(3);
+      };
+
+    // Increase score and end the game if wrong button is clicked
+    // Fasten up the game
+    
+    const clicked = (i) => {
+        if (i == active) {
+        console.log(time);
+        score++;
+        scoredisplay.textContent = `Your score is ${score}`;
+        time = time -10;
+    } else {
+        endGame();
+    }
 }
 
-const stopFunction = () => {
-    console.log("stop");
-    body.classList.add("bg");
-}
+// Game ending
 
-circleArray.forEach((el) => {
-el.addEventListener("click", clickFunction);
-});
+  };
+  const endGame = () => {
+    clearTimeout(timer);
+    console.log("Game over");
+    overlay.style.visibility = "visible";
+    gameover.textContent = `Your score is ${score}`;
+  };
 
-stopBtn.addEventListener("click", stopFunction);
+  const reloadGame = () => {
+    console.log("what?");
+    window.location.reload();
+  };
+  
+  close.addEventListener("click", reloadGame);
+
+
